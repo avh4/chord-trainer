e130fb6e363b6dfa9c72b8424c6eef76373d2d28
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-09-21 23:52:14 -0700

    Highlight current notes

--- view.elm
+++ view.elm
@@ -1,6 +1,10 @@
 import Window
+import Set
 
 -- ♭ 𝄫 ♯ 𝄪
+
+currentNotes : Signal (Set.Set String)
+currentNotes = constant (Set.fromList ["C", "E"])
 
 notes =
   [ [ "D", "G", "C", "F", "B♭", "E♭", "A♭", "D♭" ]
@@ -14,18 +18,18 @@ hexDistance = (sqrt 3)*hexRadius
 acc : (s -> s) -> (s -> a -> b) -> s -> [a] -> [b]
 acc stateFn accFn initialS list = (snd (foldl (\next (s, a) -> (stateFn s, accFn s next :: a)) (initialS, []) list))
 
-column x notes = (concat (acc (\y -> y-hexDistance) (\y note -> cell note (x * hexRadius*1.5, y - hexDistance*x/2)) (hexDistance*4) notes))
+column currentNotes x notes = (concat (acc (\y -> y-hexDistance) (\y note -> cell note (x * hexRadius*1.5, y - hexDistance*x/2) (Set.member note currentNotes)) (hexDistance*4) notes))
 
-draw w h = collage w h
-  (concat (acc (\x -> x+1) column 0 notes))
+draw w h currentNotes = collage w h
+  (concat (acc (\x -> x+1) (column currentNotes) 0 notes))
 
-cell : String -> (Float, Float) -> [Form]
-cell note pos =
-  [ move pos ((filled orange) (ngon 6 hexRadius))
+cell : String -> (Float, Float) -> Bool -> [Form]
+cell note pos sel =
+  [ move pos ((filled (if sel then yellow else orange)) (ngon 6 hexRadius))
   , move pos (outlined (solid charcoal) (ngon 6 hexRadius))
-  , move pos (toForm (plainText note))
+  , move pos (toForm ((if sel then centered (bold (toText note)) else plainText note)))
   ]
 
 
-main = lift2 draw Window.width Window.height
+main = lift3 draw Window.width Window.height currentNotes
 -- main = asText (snd (foldr (\note (x, acc) -> (x+173, (x, note) :: acc)) (-173,[]) notes))
